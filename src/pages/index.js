efb94761322cd1a0ba4db9d1419be2f91fbc8ab8
shivamsoni00 <shivajisoni04@gmail.com>
2020-03-04 12:20:06 +0530

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,301 @@
+import React from 'react'
+import style from '../pages/opy.module.css'
+const Header=()=>{
+  return(
+    <div>
+      <nav className={style.fornav}>
+        <div className={style.leftitem}>
+          <h2 className={style.head}>
+            <img className={style.i} src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/09/logo-3.png" alt=" "/>
+          </h2>
+          </div>
+          <div className={style.rightitem}>
+            <ul className={style.ylf}>
+            <li><button className={style.ut}>Lets chat</button></li>
+            <li>Contact</li>
+            <li>Blog</li>
+            <li>Price</li>
+            <li>Home</li>
+              </ul>
+          </div>
+      </nav>
+    </div>
+  )
+}
+const Page0=()=>{
+  return(
+    <div>
+      <section className={style.sec1}>
+        <h2 className={style.H2}>
+        Hello, I’m
+        </h2>
+        <h1 className={style.H1}>
+        Christian Lopez
+        </h1>
+        <h3 className={style.H3}>
+        Product Designer
+        </h3>
+      </section>
+    </div>
+  )
+}
+const Page1=()=>{
+  return(
+    <div>
+      <section className={style.sec2}>
+  <div className={style.lefthalf}>
+    <article>
+      <h1 className={style.o}>ABOUT ME</h1>
+      <h1 className={style.oo}>Why hire me for your next project?</h1>
+      <p className={style.ooo}>
+      Meet my startup design agency Shape Rex Currently I am working at CodeNext as Product Designer. Before that I’ve worked at SpurBee, Dhaka as a UX, UI Designer.
+      </p>
+      <p className={style.oooo}>
+      I love to work in User Experience & User Interface designing. Because I love to solve the design problem and find easy and better solutions to solve it. I always try my best to make good user interface with the best user experience. I have been working as a Visual designer from.
+ </p>
+ <button className={style.ooooo}>
+   LEARN MORE
+ </button>
+    </article>
+  </div>
+  <div className={style.righthalf}>
+    <article>
+      <img className={style.r} src="https://images.unsplash.com/photo-1583133183696-7e960832aa11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=" "/>
+  </article>
+  </div>
+</section>
+</div>
+  )
+}
+const Page2=(props)=>{
+  return(
+    <div className={style.pret}>
+      <h3 className={style.n}>WE PROVIDE</h3>
+      <h1 className={style.nn}>Services</h1>
+    <p className={style.nnn}>I made nine prototypes to understand family needs better and redesigned the iOS and Android apps.
+
+</p>
+
+    <div className={style.io}>
+        <div className={style.m}>
+        <div className={style.card}>
+  <div className={style.container}>
+    <img src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/05/service-1.png" alt=" "/>
+    <h4><b className={style.ss}>Business Strategy</b></h4> 
+    <p className={style.s}>Strategy develop customer in organisational knowledge and market perspectives that inform and provoke</p> 
+  </div>
+</div>
+        </div>
+        <div className={style.m}>
+        <div className={style.card}>
+  <div className={style.container}>
+    <img src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/05/service-2.png" alt=" "/>
+    <h4><b className={style.ss}>Brand Strategy</b></h4> 
+    <p className={style.s}>Strategy develop customer in organisational knowledge and market perspectives that inform and provoke.</p> 
+  </div>
+</div>
+        </div>
+        <div className={style.m}>
+        <div className={style.card}>
+  <div className={style.container}>
+    <img src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/05/service-3.png" alt=""/>
+    <h4><b className={style.ss}>Visual Design</b></h4> 
+    <p className={style.s}>Strategy develop customer in organisational knowledge and market perspectives that inform and provoke.</p> 
+  </div>
+</div>
+</div>
+
+<div className={style.m}>
+        <div className={style.card}>
+  <div className={style.container}>
+    <img src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/05/service-4.png" alt=""/>
+    <h4><b className={style.ss}>Product Design</b></h4> 
+    <p className={style.s}>Strategy develop customer in organisational knowledge and market perspectives that inform and provoke.</p> 
+  </div>
+</div>
+        </div>
+      
+        <div className={style.m}>
+        <div className={style.card}>
+  <div className={style.container}>
+    <img src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/05/service-5.png" alt=" "/>
+    <h4><b className={style.ss}>Interactive Design</b></h4> 
+    <p className={style.s}>Strategy develop customer in organisational knowledge and market perspectives that inform and provoke.</p> 
+  </div>
+</div>
+        </div>
+       
+       
+        <div className={style.m}>
+        <div className={style.card}>
+  <div className={style.container}>
+    <img src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/05/service-6.png" alt=" "/>
+    <h4><b className={style.ss}>Motion Graphic</b></h4> 
+  <p className={style.s}>Strategy develop customer in organisational knowledge and market perspectives that inform and provoke.</p> 
+  </div>
+</div>
+        </div>
+        </div>
+    </div>
+      
+    
+  )
+}
+const Page3=()=>{
+  return(
+    <div className={style.pret1}>
+      <div className={style.v}>
+        <p className={style.vv}>MY WORKS</p>
+        <h1 className={style.vvv}>projects</h1>
+      </div>
+      <ul className={style.pro}>
+        <li>ALL-WORKS</li>
+        <li>APP DESIGN</li>
+        <li>BRANDING</li>
+        <li>INTERIOR</li>
+        <li>PRODUCT-DESIGN</li>
+        <li>WEB-DESIGN</li>
+      </ul>
+
+      <ul className={style.pro1}>
+        <li><img className={style.x} src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/06/work-1.jpg" alt=""/></li>
+        <li><img className={style.x} src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/06/work-14.jpg" alt=""/></li>
+        <li><img className={style.x} src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/06/work-3.jpg" alt=""/></li>
+        <li><img className={style.x} src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/06/work-10.jpg" alt=""/></li>
+        <li><img className={style.x} src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/06/work-4.jpg" alt=""/></li>
+        <li><img className={style.x} src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/06/work-12.jpg" alt=""/></li>
+        </ul>
+
+</div>
+  )
+}
+
+const Page4=()=>{
+  return(
+    <div className={style.pret2}>
+      <div className={style.z}>
+        <p className={style.zz}>TESTIMONIALS</p>
+        <h1 className={style.zzz}>Happy Clients Says</h1>
+        </div>
+      <div className={style.zzzz}>
+      <div className={style.zzzzz}>    
+  <div className={style.zzzzzz}>
+    <img className={style.im} src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/06/team-1-300x300.png" alt=""/>
+    <h2 className={style.imm}>shivam</h2>
+    <p className={style.iim}>product designer</p>
+    <p className={style.iiim}>
+    Strategy develop customer in organisational knowledge and market perspectives that inform and provoke.and what u give us for emoityon asd tum bhi so pawog and achna apac deverel with community standrads
+    </p>
+</div>
+</div> 
+</div>
+<div className={style.zzzz}>
+      <div className={style.zzzzz}>    
+  <div className={style.zzzzzz}>
+    <img className={style.im} src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/06/team-1-300x300.png" alt=""/>
+    <h2 className={style.imm}>shivam</h2>
+    <p className={style.iim}>product designer</p>
+    <p className={style.iiim}>
+    Strategy develop customer in organisational knowledge and market perspectives that inform and provoke.and what u give us for emoityon asd tum bhi so pawog and achna apac deverel with community standrads
+    </p>
+</div>
+</div> 
+</div>
+</div>
+  )
+}
+
+
+const Page5=()=>{
+    return(
+      <div classname={style.pret3}>
+        <div className={style.aise}>
+          <div className ={style.wq}>
+            <p className={style.t}>our story</p>
+            <h2 className={style.tt}>Blog</h2>
+          </div>
+        <div className={style.a}>
+          <div className={style.aa}>
+            <img className={style.ao} src ="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/05/post-7.jpg" alt=" "/>
+            <h2 className={style.aoo}>REASON WHY U NEED </h2>
+            <p className ={style.aq}>
+            It’s been quite a while since talked about our failures here. Since then, we’ve made some new mistakes but used each of them as an opportunity to grow and we would like to share with you the lessons we learned.
+            </p>
+            <button className={style.aw}>READ MORE</button>
+          </div>
+        </div>
+
+        <div className={style.a}>
+          <div className={style.aa}>
+            <img className={style.ao} src ="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/05/post-7.jpg" alt=" "/>
+            <h2 className={style.aoo}>REASON WHY U NEED </h2>
+            <p className ={style.aq}>
+            It’s been quite a while since talked about our failures here. Since then, we’ve made some new mistakes but used each of them as an opportunity to grow and we would like to share with you the lessons we learned.
+            </p>
+            <button className={style.aw}>READ MORE</button>
+          </div>
+        </div>
+
+        <div className={style.a}>
+          <div className={style.aa}>
+            <img className={style.ao} src ="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/05/post-7.jpg" alt=" "/>
+            <h2 className={style.aoo}>REASON WHY U NEED </h2>
+            <p className ={style.aq}>
+            It’s been quite a while since talked about our failures here. Since then, we’ve made some new mistakes but used each of them as an opportunity to grow and we would like to share with you the lessons we learned.
+            </p>
+            <button className={style.aw}>READ MORE</button>
+          </div>
+        </div>
+
+        <div className={style.a}>
+          <div className={style.aa}>
+            <img className={style.ao} src ="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/05/post-7.jpg" alt=" "/>
+            <h2 className={style.aoo}>REASON WHY U NEED </h2>
+            <p className ={style.aq}>
+            It’s been quite a while since talked about our failures here. Since then, we’ve made some new mistakes but used each of them as an opportunity to grow and we would like to share with you the lessons we learned.
+            </p>
+            <button className={style.aw}>READ MORE</button>
+          </div>
+        </div>
+</div>
+</div>
+    )
+  }
+const Page6=()=>{
+  return(
+    <div>
+<div className={style.q}>
+<h2 className={style.qq}>
+            <img className={style.qqq} src="https://spaceraceit.com/wp/growth/home2/wp-content/uploads/sites/5/2019/09/logo-3.png" alt=" "/>
+          </h2>
+     <h2 className={style.qi}>Let's Talk</h2>   
+     <button className={style.qii}>LETS CHAT</button>  
+</div>
+<div className={style.w}>
+  <ul className={style.ww}>
+    <li>Home</li>
+    <li>Price</li>
+    <li>Blog</li>
+    <li>contact</li>
+  </ul>
+  <p className={style.y}>Copyright By@Example - 2019</p>
+</div>
+    </div>
+  )
+
+}
+const App=()=>{
+  return(
+    <div>
+      <Header/>
+      <Page0/>
+      <Page1/>
+      <Page2/>
+      <Page3/>
+      <Page4/>
+      <Page5/>
+      <Page6/>
+</div>
+       )
+}
+export default App
